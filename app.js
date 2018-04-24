0745fb6d9bfb3c129df7a8f4a7af4d3550f770d1
Ed Brook <edbrook@gmail.com>
2018-04-24 15:27:54 +0100

    add CORS headers

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/soa/api', soaApiRouter);
 
 // catch 404 and forward to error handler
