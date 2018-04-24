b1af581df514f8b925ad04749cbbedc19856fbba
Ed Brook <edbrook@gmail.com>
2018-04-24 12:37:15 +0100

    completed initial service

--- routes/soa-api.js
+++ routes/soa-api.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  const hello = 'world';
-  res.json({ hello });
+const ELEC_PRICE_PER_KWH = 15.4;
+
+const GAS_CALORIFIC_VALUE = 39.8;
+const GAS_VOL_CORRECTION = 1.02264;
+const GAS_CAL_TO_KHW = 3.6;
+const GAS_PRICE_PER_KWH = 8.276;
+
+const gasUnitsToKwH = units => (
+  units
+    * GAS_CALORIFIC_VALUE
+    * GAS_VOL_CORRECTION
+    / GAS_CAL_TO_KHW);
+
+router.post('/reading/gas', (req, res) => {
+  const { units } = req.body;
+  const kwh = gasUnitsToKwH(units);
+  const cost = Math.round(GAS_PRICE_PER_KWH * kwh) / 100;
+  res.json({ cost, kwh });
+});
+
+router.post('/reading/electricity', (req, res) => {
+  const { units } = req.body;
+  const cost = Math.round(units * ELEC_PRICE_PER_KWH) / 100;
+  res.json({ cost, kwh: units });
 });
 
 module.exports = router;
